b15b48b8c82e7d36ddb44c523505b27a5546f19e
Karol Marcjan <karol.marcjan@gmail.com>
2014-04-15 11:14:05 +0200

    Start Qt application

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python2
 # -*- encoding: utf-8 -*-
 
+import sys
+
 from PyQt4 import QtGui, QtCore
 
 
 if __name__ == '__main__':
-    pass
+
+    app = QtGui.QApplication(sys.argv)
+
+    sys.exit(app.exec_())
